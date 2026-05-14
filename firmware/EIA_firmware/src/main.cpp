253cb6ba2b2755576c7c0422b84f314e72772673
JozP03 <jakubkanapka@wp.pl>
2026-05-14 18:57:25 +0200

    Add USB serial WiFi setup and ESP32 firmware

--- firmware/EIA_firmware/src/main.cpp
+++ firmware/EIA_firmware/src/main.cpp
@@ -1,18 +1,77 @@
-#include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Definicja timeoutu dla połączenia (15 sekund)
+const int WIFI_TIMEOUT_MS = 15000;
+
+void executeWifiScan();
+void handleConnectionRequest(String cmd);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  
+  delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    input.trim(); 
+
+    if (input == "SCAN") {
+      executeWifiScan();
+    } 
+    else if (input.startsWith("CONN:")) {
+      handleConnectionRequest(input);
+    }
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void executeWifiScan() {
+  int n = WiFi.scanNetworks();
+  
+  if (n == 0) {
+    Serial.println("NETWORKS:BRAK_SIECI");
+  } else {
+    String response = "NETWORKS:";
+    for (int i = 0; i < n; ++i) {
+      response += WiFi.SSID(i);
+      if (i < n - 1) {
+        response += ",";
+      }
+    }
+    Serial.println(response);
+  }
+  
+  WiFi.scanDelete();
+}
+
+void handleConnectionRequest(String cmd) {
+  int separatorIndex = cmd.indexOf(';');
+  
+  if (separatorIndex == -1) {
+    Serial.println("STATUS:ERROR_FORMAT");
+    return;
+  }
+
+  String ssid = cmd.substring(5, separatorIndex);
+  String password = cmd.substring(separatorIndex + 1);
+
+  WiFi.begin(ssid.c_str(), password.c_str());
+
+  unsigned long startAttemptTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < WIFI_TIMEOUT_MS) {
+    delay(500);
+  }
+
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.print("STATUS:OK;IP:");
+    Serial.println(WiFi.localIP().toString());
+  } else {
+    Serial.println("STATUS:ERROR_TIMEOUT");
+    WiFi.disconnect();
+  }
 }
