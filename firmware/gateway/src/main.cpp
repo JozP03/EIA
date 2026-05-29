fccff7e7ff73b36ae3d41a12b036a2e75e245a5a
JozP03 <jakubkanapka@wp.pl>
2026-05-29 14:18:52 +0200

    Add MQTT/TLS support using PubSubClient

--- firmware/gateway/src/main.cpp
+++ firmware/gateway/src/main.cpp
@@ -1,21 +1,76 @@
 #include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
 
 const int WIFI_TIMEOUT_MS = 25000;
+
+//mqtt zmienne
+const char* mqtt_server = "";
+const int mqtt_port = 8883;
+const char* mqtt_user = "";
+const char* mqtt_pass = "";
+
+//certyfikat ISRG Root X1 https://letsencrypt.org/certificates/
+const char* root_ca = \
+  "-----BEGIN CERTIFICATE-----\n" \
+  "MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw" \
+  "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh" \
+  "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4" \
+  "WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu" \
+  "ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY" \
+  "MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc" \
+  "h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+" \
+  "0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U" \
+  "A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW" \
+  "T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH" \
+  "B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC" \
+  "B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv" \
+  "KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn" \
+  "OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn" \
+  "jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw" \
+  "qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI" \
+  "rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV" \
+  "HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq" \
+  "hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL" \
+  "ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ" \
+  "3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK" \
+  "NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5" \
+  "ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur" \
+  "TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC" \
+  "jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc" \
+  "oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq" \
+  "4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA" \
+  "mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d" \
+  "emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=" \
+  "-----END CERTIFICATE-----\n";
+
+WiFiClientSecure espClient;
+PubSubClient mqttClient(espClient);
 
 void executeWifiScan();
 void handleConnectionRequest(String cmd);
 void handleStaticConnectionRequest(String cmd);
+void testMqttConnection();
+void mqttCallback(char* topic, byte* payload, unsigned int length);
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600); //zmienic pozniej na 115200
   
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
   
+  espClient.setCACert(root_ca);
+  mqttClient.setServer(mqtt_server, mqtt_port);
+  mqttClient.setCallback(mqttCallback);
   delay(1000);
 }
 
 void loop() {
+
+  if (WiFi.status() == WL_CONNECTED && mqttClient.connected()) {
+    mqttClient.loop();
+  }
+
   if (Serial.available() > 0) {
     String input = Serial.readStringUntil('\n');
     input.trim();
@@ -54,21 +109,19 @@ void executeWifiScan() {
       String protectedStr = isProtected ? "1" : "0";
 
       // Format: NAZWA_SIECI,RSSI,CZY_ZABEZPIECZONA
-      // Przykład: MojeSuperWiFi,-65,1
+      // Przykład: nameWiFi,-65,1
       Serial.print(ssid);
       Serial.print(",");
       Serial.print(rssi);
       Serial.print(",");
       Serial.println(protectedStr);
       
-      delay(10); // Małe opóźnienie, żeby nie zapchać bufora Seriala
+      delay(10);
     }
   }
   
-  // Informujemy Androida, że to już koniec listy
   Serial.println("SCAN_FINISHED");
   
-  // Czyszczenie pamięci po skanowaniu
   WiFi.scanDelete();
 }
 
@@ -95,6 +148,8 @@ void handleConnectionRequest(String cmd) {
   if (WiFi.status() == WL_CONNECTED) {
     Serial.print("STATUS:OK;IP:");
     Serial.println(WiFi.localIP().toString());
+    delay(1000);
+    testMqttConnection();
   } else {
     Serial.println("STATUS:ERROR_TIMEOUT");
     WiFi.disconnect();
@@ -160,5 +215,38 @@ void handleStaticConnectionRequest(String cmd) {
   } else {
     Serial.println("STATUS:ERROR_TIMEOUT");
     WiFi.disconnect();
+  }
+}
+
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("MQTT_RCV:[");
+  Serial.print(topic);
+  Serial.print("]:");
+  for (unsigned int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void testMqttConnection() {
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("STATUS:MQTT_ERROR_NO_WIFI");
+    return;
+  }
+
+  Serial.println("STATUS:MQTT_CONNECTING");
+  
+  // Próba połączenia z chmurą
+  if (mqttClient.connect("Gateway_Test", mqtt_user, mqtt_pass)) {
+    Serial.println("STATUS:MQTT_OK");
+    
+    // testowa wiadomość
+    mqttClient.publish("dom/brama/status", "{\"brama\":\"online\"}");
+    
+    mqttClient.subscribe("#");
+    Serial.println("STATUS:MQTT_SUBSCRIBED");
+  } else {
+    Serial.print("STATUS:MQTT_ERROR_CODE:");
+    Serial.println(mqttClient.state());
   }
 }
