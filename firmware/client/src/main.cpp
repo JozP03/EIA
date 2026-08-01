0ec806255be1b9e01f002e2a8dfef246f0bfac98
JozP03 <jakubkanapka@wp.pl>
2026-08-01 14:37:12 +0200

    Add structured sensor handling and dynamic BLE payload

--- firmware/client/src/main.cpp
+++ firmware/client/src/main.cpp
@@ -19,6 +19,34 @@ Adafruit_AHTX0 aht;
 bool hasBMP = false;
 bool hasAHT = false;
 
+struct Metric {
+  String prefix;
+  String unit;
+  float value;
+  int decimals;
+};
+
+struct SensorDevice {
+  uint8_t address;
+  String name;
+  bool isActive;
+  int metricCount;
+  Metric metrics[2];
+};
+
+SensorDevice mySensors[2] = {
+  { 
+    0x38, "AHT20", false, 2, 
+    { {"T", "C", 0.0, 1}, {"H", "%", 0.0, 0} }
+  },
+  { 
+    0x76, "BMP280", false, 1, 
+    { {"P", "hPa", 0.0, 0}, {"", "", 0.0, 0} }
+  }
+};
+
+const int numSensors = sizeof(mySensors) / sizeof(mySensors[0]);
+
 BLEAdvertising *pAdvertising;
 float mockTemperature = 22.0;
 
@@ -35,20 +63,35 @@ bool checkI2C(uint8_t address) {
 void setId() {
   String mac = WiFi.macAddress();
   mac.replace(":", "");
-  uniqueSensorName = "ESP_" + mac.substring(mac.length() - 6);
+  uniqueSensorName = "ESP_" + mac.substring(mac.length() - 4);
 }
 
 void setup() {
   Serial.begin(115200);
   setId();
 
+  Wire.begin(I2C_SDA, I2C_SCL);
+
+  for (int i = 0; i < numSensors; i++) {
+    if (checkI2C(mySensors[i].address)) {
+      Serial.println("Wykryto: " + mySensors[i].name);
+      
+      if (mySensors[i].address == 0x38) {
+        mySensors[i].isActive = aht.begin(&Wire);
+      } 
+      else if (mySensors[i].address == 0x76) {
+        mySensors[i].isActive = bmp.begin(0x76);
+      }
+    } else {
+      Serial.println("Brak: " + mySensors[i].name);
+    }
+  }
+
   BLEDevice::init("");
   pAdvertising = BLEDevice::getAdvertising();
   
   pAdvertising->setScanResponse(true); 
   pAdvertising->start();
-  
-  Serial.println("Start rozgłaszania...");
 }
 
 void loop() {
@@ -57,19 +100,42 @@ void loop() {
   if (currentTime - lastSendTime >= sendInterval) {
     lastSendTime = currentTime;
 
-    mockTemperature += 0.1;
-    if(mockTemperature > 28.0) mockTemperature = 22.0;
+    for (int i = 0; i < numSensors; i++) {
+      if (mySensors[i].isActive) {
+        if (mySensors[i].address == 0x38) { // AHT
+          sensors_event_t humidity, temp;
+          aht.getEvent(&humidity, &temp);
+          mySensors[i].metrics[0].value = temp.temperature;
+          mySensors[i].metrics[1].value = humidity.relative_humidity;
+        } 
+        else if (mySensors[i].address == 0x76) { // BMP
+          mySensors[i].metrics[0].value = bmp.readPressure() / 100.0F;
+        }
+      }
+    }
 
-    String payload = "ID:" + uniqueSensorName + ";T:" + String(mockTemperature, 1) + ";U:" + String(Defunit);
+    String payload = uniqueSensorName;
+    bool anyData = false;
     
+    for (int i = 0; i < numSensors; i++) {
+      if (mySensors[i].isActive) {
+        for (int m = 0; m < mySensors[i].metricCount; m++) {
+          payload += ";" + mySensors[i].metrics[m].prefix + ":" + String(mySensors[i].metrics[m].value, mySensors[i].metrics[m].decimals);
+          anyData = true;
+        }
+      }
+    }
+
+    if (!anyData) {
+      payload += ";ERR:NoSensors";
+    }
+
     pAdvertising->stop();
 
-    // 1. Pakiet główny 
     BLEAdvertisementData advertisementData;
     advertisementData.setFlags(ESP_BLE_ADV_FLAG_GEN_DISC | ESP_BLE_ADV_FLAG_BREDR_NOT_SPT);
     advertisementData.setManufacturerData(payload.c_str()); 
     
-    // 2. Scan Response
     BLEAdvertisementData scanResponseData;
     scanResponseData.setName(uniqueSensorName.c_str());
 
