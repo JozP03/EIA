9b46a7f2c31fa480fc229c8a5e4a31452a09af26
JozP03 <jakubkanapka@wp.pl>
2026-08-13 20:51:47 +0200

    Add Room database dependencies

--- application/app/build.gradle.kts
+++ application/app/build.gradle.kts
@@ -51,6 +51,12 @@ dependencies {
     implementation(libs.hivemq.mqtt.client)
     implementation(libs.retrofit)
     implementation(libs.converter.gson)
+
+    // Room
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler)
+    implementation(libs.room.ktx)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.espresso.core)
     androidTestImplementation(libs.ext.junit)
